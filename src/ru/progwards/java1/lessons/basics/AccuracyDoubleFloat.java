2ea7b7c5c561730600cc2898d9d8831373d73133
Student <umk27@mail.ru>
2020-06-21 11:01:45 +0300

    Homework3

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -4,13 +4,13 @@ public class AccuracyDoubleFloat {
 
     public static void main(String[] args) {
         System.out.println(calculateAccuracy(6371.2));
-        System.out.println(volumeBallDouble(1));
-        System.out.println(volumeBallFloat(1));
+        System.out.println(volumeBallDouble(6371.2));
+        System.out.println(volumeBallFloat(6371.2f));
     }
 
     public static double volumeBallDouble(double radius) {
-        double pi =3.14f;
-        return 4 / 3 * pi * Math.pow(radius, 3); //111
+        float pi = 3.14f;
+        return 4f / 3f * pi * Math.pow(radius, 3);
 
     }
 
