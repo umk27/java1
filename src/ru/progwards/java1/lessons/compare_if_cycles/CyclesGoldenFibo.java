0ed30aa512ef5061a0a0d00744b106ba7bc8f783
Student <umk27@mail.ru>
2020-06-24 21:19:35 +0300

    Homework4

---
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -0,0 +1,59 @@
+package ru.progwards.java1.lessons.compare_if_cycles;
+
+public class CyclesGoldenFibo {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 15; i++) {
+            System.out.println(fiboNumber(i));
+        }
+
+        for (int x = 1; x <= 100; x++) {
+            for (int y = 1; y <= 100; y++) {
+                for (int z = 1; z <= 100; z++) {
+                    boolean c = isGoldenTriangle(x, y, z);
+                    if (c == true) System.out.println("Сторона x= " + x + " Сторона y= " + y + " Сторона z= " + z);
+                }
+            }
+        }
+    }
+
+    public static boolean containsDigit(int number, int digit) {
+        char a;
+        String a1;
+        int a2;
+        String number1 = Integer.toString(number);
+        for (int i = 0; i != 10; i++) {
+            if (i >= number1.length()) break;
+            System.out.println(i);
+            a = number1.charAt(i);
+            a1 = String.valueOf(a);
+            System.out.println(a1);
+            a2 = Integer.parseInt(a1);
+            if (a2 == digit) return true;
+        }
+        return false;
+    }
+
+    public static int fiboNumber(int n) {
+        int a = 1;
+        int b = 1;
+        if (n == 1 || n == 2) return 1;
+        int c = 0;
+        for (int i = 2; i != n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+
+    public static boolean isGoldenTriangle(int a, int b, int c) {
+        float a1 = (float) a;
+        float b1 = (float) b;
+        float c1 = (float) c;
+        if (a == b) if (1.61703 < a1 / c1 && a1 / c1 < 1.61903) return true;
+        if (a == c) if (1.61703 < a1 / b1 && a1 / b1 < 1.61903) return true;
+        if (b == c) if (1.61703 < b1 / a1 && b1 / a1 < 1.61903) return true;
+        return false;
+    }
+}
---
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -0,0 +1,25 @@
+package ru.progwards.java1.lessons.compare_if_cycles;
+
+public class TriangleInfo {
+
+    public static void main(String[] args) {
+        System.out.println(isTriangle(3,6,5));
+        System.out.println(isRightTriangle(3, 6, 5));
+        System.out.println(isIsoscelesTriangle(3, 3, 5));
+    }
+
+    public static boolean isTriangle(int a, int b, int c) {
+        if (a + b > c || a + c > b || b + c > a) return true;
+        else return false;
+    }
+
+    public static boolean isRightTriangle(int a, int b, int c) {
+        if (a*a+b*b==c*c || a*a+c*c==b*b || b*b+c*c==a*a) return true;
+        else return false;
+    }
+
+    public static boolean isIsoscelesTriangle(int a, int b, int c) {
+        if (a==b || a==c || b==c) return true;
+        else return false;
+    }
+}
