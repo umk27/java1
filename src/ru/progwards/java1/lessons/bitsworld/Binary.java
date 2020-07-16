049d2f45d1b7409c19ab327c7208e63cd10088e3
Student <umk27@mail.ru>
2020-07-16 23:23:11 +0300

    Homework7

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -15,82 +15,11 @@ public class Binary {
 
     @Override
     public String toString() {
-        int i = 0;
         String s = "";
-        byte num1 = 0;
-        if (num >= 0) {
-            num1 = num;
-        } else {
-            num1 = (byte) (128 + num);
+        for (int i = 1; i <= 8; i++) {
+            s += Integer.toString(num & 1);
+            num >>= (byte) 1;
         }
-        System.out.println(num1);
-        while (num1 != 0) {
-            s = Integer.toString(num1 % 2) + s;
-            System.out.println(s);
-            num1 = (byte) (num1 / (byte) 2);
-            System.out.println(num1);
-            i++;
-            System.out.println(i);
-        }
-
-        if (num >= 0) {
-
-            switch (i) {
-                case 7:
-                    s = "0" + s;
-                    break;
-                case 6:
-                    s = "00" + s;
-                    break;
-                case 5:
-                    s = "000" + s;
-                    break;
-                case 4:
-                    s = "0000" + s;
-                    break;
-                case 3:
-                    s = "00000" + s;
-                    break;
-                case 2:
-                    s = "000000" + s;
-                    break;
-                case 1:
-                    s = "0000000" + s;
-                    break;
-                case 0:
-                    s = "00000000";
-            }
-        }
-
-        else {
-
-            switch (i) {
-                case 7:
-                    s = "1" + s;
-                    break;
-                case 6:
-                    s = "10" + s;
-                    break;
-                case 5:
-                    s = "100" + s;
-                    break;
-                case 4:
-                    s = "1000" + s;
-                    break;
-                case 3:
-                    s = "10000" + s;
-                    break;
-                case 2:
-                    s = "100000" + s;
-                    break;
-                case 1:
-                    s = "1000000" + s;
-                    break;
-                case 0:
-                    s = "10000000";
-            }
-        }
-
         return s;
     }
 }
--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -3,20 +3,18 @@ package ru.progwards.java1.lessons.bitsworld;
 public class SumBits {
 
     public static void main(String[] args) {
-        System.out.println(sumBits((byte) 0b0101011));
+        System.out.println(sumBits((byte) 0b10000000));
 
     }
 
     public static int sumBits(byte value) {
         int a = 0;
-        for (int i = 0; value != 0; i++) {
+        for (int i = 1; i <= 8; i++) {
             a += value & 1;
             value >>= (byte) 1;
-            if (i > 8) {
-                break;
-            }
-            System.out.println(i);
         }
         return a;
     }
 }
+
+
