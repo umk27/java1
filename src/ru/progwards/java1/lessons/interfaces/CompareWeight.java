f35bd1dd3ae00b0155be8141928a7d39b99feb50
Student <umk27@mail.ru>
2020-07-25 18:55:12 +0300

    Homework8

--- src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
+++ src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
@@ -1,12 +1,12 @@
 package ru.progwards.java1.lessons.interfaces;
 
-public interface CompareWeight<CompareWeight> {
+public interface CompareWeight<ArraySort> {
     public enum CompareResult {
         LESS,
         EQUAL,
         GREATER
     }
 
-    public CompareResult compareWeight(CompareWeight smthHasWeigt);
+    public CompareResult compareWeight(ArraySort smthHasWeigt);
 
 }
