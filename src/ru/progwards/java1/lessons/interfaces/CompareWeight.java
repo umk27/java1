1d9af6518c78f108b8082cfabc3bc080108e3eb1
Student <umk27@mail.ru>
2020-07-26 12:08:22 +0300

    Homework8

--- src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
+++ src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
@@ -11,16 +11,5 @@ public interface CompareWeight<CompareWeight> {
 
     public CompareResult compareWeight(CompareWeight smthHasWeigt);
 
-    public static void sort(CompareWeight[] a) {
-        for (int i = 0; i < a.length; i++) {
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[i].compareWeight<CompareWeight>(a[j])==CompareResult.GREATER) {
-                    CompareWeigh b = a[i];
-                    a[i] = a[j];
-                    a[j] = b;
-                }
-            }
-        }
-        System.out.println(Arrays.toString(a));
-    }
+    public void sort(CompareWeight[] a);
 }
