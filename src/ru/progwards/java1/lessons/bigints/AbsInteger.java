54d1d0858489138233b309e3832ef7d6a9bc4da1
Student <umk27@mail.ru>
2020-09-25 19:26:27 +0300

    Homework9

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -1,18 +1,24 @@
 package ru.progwards.java1.lessons.bigints;
 
+import java.math.BigInteger;
+
 public class AbsInteger {
 
 
     static public AbsInteger add(AbsInteger num1, AbsInteger num2) {
-        int z = num1.toInt() + num2.toInt();
-        if (-128 < z && z < 127) {
-            AbsInteger ab = new ByteInteger((byte) z);
+        BigInteger a = new BigInteger(String.valueOf(num1.toInt()));
+        BigInteger b = new BigInteger(String.valueOf(num2.toInt()));
+        BigInteger z = a.add(b);
+        String z1 = String.valueOf(z);
+        int z2 = Integer.parseInt(z1);
+        if (-128 < z2 && z2 < 127) {
+            AbsInteger ab = new ByteInteger((byte) z2);
             return ab;
-        } else if (-32768 < z && z < 32767) {
-            AbsInteger as = new ShortInteger((short) z);
+        } else if (-32768 < z2 && z2 < 32767) {
+            AbsInteger as = new ShortInteger((short) z2);
             return as;
         } else {
-            AbsInteger ai = new IntInteger(z);
+            AbsInteger ai = new IntInteger(z2);
             return ai;
         }
     }
