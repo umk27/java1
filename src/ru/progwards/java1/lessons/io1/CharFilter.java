82d94dc8d9a591ff45259521dce14f13419d2dbb
Student <umk27@mail.ru>
2020-10-02 20:25:21 +0300

    Homework10

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class CharFilter {
 
     public static void main(String[] args) {
-        filterFile("Text1.txt", "Text2.txt", "-,.()");
+        filterFile("Text1.txt", "Text2.txt", "-,JSO");
     }
 
     public static void filterFile(String inFileName, String outFileName, String filter) {
@@ -19,20 +19,11 @@ public class CharFilter {
             FileReader reader = new FileReader(inFileName);
             Scanner scanner = new Scanner(reader);
             while (scanner.hasNextLine()) {
-                String a = scanner.nextLine();
-                char[] oldcode = a.toCharArray();
-                for (int j = 0; j < f.length; j++) {
-                    String q = String.valueOf(f[j]);
-                    for (int i = 0; i < oldcode.length; i++) {
-                        String w = String.valueOf(oldcode[i]);
-                        if (w.equals(q)) {
-                            for (int k = i; i < oldcode.length - 1 - i; i++) {
-                                oldcode[k] = oldcode[k + 1];
-                            }
-                        }
-                    }
+                z1 = scanner.nextLine();
+                for (int i = 0; i < f.length; i++) {
+                    String b = String.valueOf(f[i]);
+                    z1 = z1.replaceAll(b, "");
                 }
-                z1 = new String(oldcode);
                 z = z + z1 + "\n";
                 System.out.println(z);
             }
