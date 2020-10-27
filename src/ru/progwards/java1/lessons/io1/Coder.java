0f1976ac90b684ec1d0c1354c8db9e914f73400a
Student <umk27@mail.ru>
2020-10-27 21:04:40 +0300

    Homework10

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -15,17 +15,9 @@ import java.util.Scanner;
 public class Coder {
 
     public static void main(String[] args) {
-        byte b = 13;
-        char c = (char) b;
-        System.out.println("char c="+c);
-        int i = Integer.valueOf(c);
-        System.out.println("int i="+i);
-        String s = String.valueOf(c);
-        System.out.println("String s="+s);
-        int ii = Integer.parseInt(s);
-        System.out.println("int ii="+ii);
-     //   char[] code = {'9', '8', '7', '6', '5', '4', '3', '2', '1', '0'};
-     //   codeFile("t1.txt", "t2.txt", code, "t3.txt");
+
+        char[] code = {'9', '8', '7', '6', '5', '4', '3', '2', '1', '0'};
+        codeFile("t1.txt", "t2.txt", code, "t3.txt");
     }
 
     public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
@@ -34,29 +26,15 @@ public class Coder {
         try {
             FileInputStream bytefile = new FileInputStream(inFileName);
             byte[] b = bytefile.readAllBytes();
-            System.out.println(Arrays.toString(b));
             char[] c = new char[b.length];
             char[] code1 = new char[b.length];
             for (int i = 0; i < b.length - 2; i++) {
-                if (i != 4 && i != 5) {
-                    c[i] = (char) b[i];
-                    //System.out.println(c[i]);
-                    Double d = Double.valueOf(c[i]);
-                    System.out.println(d);
-                    BigDecimal bd = new BigDecimal(c[i]);
-                    BigInteger bi = bd.toBigInteger();
-                    Integer q = bi.intValue();
-                    String w = String.valueOf(c[i]);
-                    //System.out.println(w);
-                    // code1[i] = code[c[i]];
-                 //   System.out.println(code[i]);
-                }
+                code1[i] = code[b[i]];
             }
             z = new String(code1);
             System.out.println(z);
             bytefile.close();
-        } catch (
-                Exception e) {
+        } catch (Exception e) {
             try {
                 FileWriter writer1 = new FileWriter(logName);
                 writer1.write(e.getMessage());
