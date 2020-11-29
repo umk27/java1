0a938decaab59d888dfd241b4e94dcda9afa74e5
Student <umk27@mail.ru>
2020-11-29 16:17:15 +0300

    Homework12

--- src/ru/progwards/java1/lessons/collections/ArrayIterator.java
+++ src/ru/progwards/java1/lessons/collections/ArrayIterator.java
@@ -2,24 +2,32 @@ package ru.progwards.java1.lessons.collections;
 
 import java.util.Iterator;
 
-    public class ArrayIterator<T> implements Iterator<T> {
+public class ArrayIterator<T> implements Iterator<T> {
 
-        private T[] array;
-
-        ArrayIterator(T[] array) {
-            this.array = array;
-        }
-
-        @Override
-        public boolean hasNext() {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        @Override
-        public T next() {
-            // TODO Auto-generated method stub
-            return null;
+    public static void main(String[] args) {
+        ArrayIterator<Integer> arrayIterator = new ArrayIterator<>(new Integer[]{1, 2, 3, 4, 5});
+        while (arrayIterator.hasNext()) {
+            System.out.println(arrayIterator.next());
         }
     }
+
+    private T[] array;
+    private int a;
+
+    ArrayIterator(T[] array) {
+        this.array = array;
+    }
+
+    @Override
+    public boolean hasNext() {
+
+        return a < array.length;
+    }
+
+    @Override
+    public T next() {
+
+        return array[a++];
+    }
+}
 
