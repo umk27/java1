7294bff793b0fdce3a92314b6983d026e5fea3bb
Student <umk27@mail.ru>
2020-12-19 15:42:10 +0300

    Homework16

---
+++ src/ru/progwards/java1/lessons/datetime/SessionManager.java
@@ -0,0 +1,10 @@
+package ru.progwards.java1.lessons.datetime;
+
+import java.time.LocalDateTime;
+
+public class SessionManager {
+
+    private int sessionHandle;
+    private String userName;
+    private LocalDateTime lastAccess;
+}
