1b156367b1f87800a43172c6b57ae22454de9d46
Student <umk27@mail.ru>
2020-12-27 19:58:41 +0300

    Homework16

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -13,19 +13,58 @@ public class Profiler {
             Thread.sleep(100);
         } catch (InterruptedException e) {
         }
+        exitSection("Process1");
+
+        enterSection("Process1");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
         enterSection("Process2");
         try {
             Thread.sleep(200);
         } catch (InterruptedException e) {
         }
+        enterSection("Process3");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+        exitSection("Process3");
         exitSection("Process2");
         enterSection("Process2");
         try {
             Thread.sleep(200);
         } catch (InterruptedException e) {
         }
+        enterSection("Process3");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+        exitSection("Process3");
+        exitSection("Process2");
+        enterSection("Process2");
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+        }
+        enterSection("Process3");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+        exitSection("Process3");
         exitSection("Process2");
         exitSection("Process1");
+
+        enterSection("Process1");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+        exitSection("Process1");
+
         String info = "";
         for (StatisticInfo si : getStatisticInfo()) {
             info += si.sectionName + " " + (si.fullTime / 100 * 100) + " " + (si.selfTime / 100 * 100) + " " + si.count + "\n";
@@ -68,6 +107,8 @@ public class Profiler {
     public static List<StatisticInfo> getStatisticInfo() {
         List<StatisticInfo> list = new ArrayList<>();
         for (int i = 0; i < names.size(); i++) {
+            ArrayList<Long> r1 = new ArrayList<>();
+            ArrayList<Long> r2 = new ArrayList<>();
             StatisticInfo statisticInfo = new StatisticInfo();
             statisticInfo.sectionName = names.get(i);
             ArrayList<Instant> i1 = enter.get(names.get(i));
@@ -77,21 +118,36 @@ public class Profiler {
             for (int j = 0; j < i1.size(); j++) {
                 long a = Duration.between(i1.get(j), i2.get(j)).toMillis();
                 statisticInfo.fullTime = (int) (statisticInfo.fullTime + a);
-                statisticInfo.selfTime = statisticInfo.fullTime;
+                if (j == 0) {
+                    statisticInfo.selfTime = statisticInfo.fullTime;
+                } else {
+                    statisticInfo.selfTime = (int) (statisticInfo.selfTime + a);
+                }
                 for (int v = i + 1; v < names.size(); v++) {
                     ArrayList<Instant> i11 = enter.get(names.get(v));
                     ArrayList<Instant> i22 = exit.get(names.get(v));
                     for (int x = 0; x < i11.size(); x++) {
-                      //  long q1 = i1.get(j).toEpochMilli();
-                      //  long q11 = i11.get(x).toEpochMilli();
-                      //  long w1 = i22.get(x).toEpochMilli();
-                      //  long w11 = i2.get(j).toEpochMilli();
-                        if (i1.get(j).toEpochMilli() <= i11.get(x).toEpochMilli() && i22.get(x).toEpochMilli() <= i2.get(j).toEpochMilli()) {
+                        long q1 = i1.get(j).toEpochMilli();
+                        long q11 = i11.get(x).toEpochMilli();
+                        long w11 = i22.get(x).toEpochMilli();
+                        long w1 = i2.get(j).toEpochMilli();
+                        boolean m = true;
+                        for (int n = 0; n < r1.size(); n++) {
+                            if (r1.get(n) <= q11 && w11 <= r2.get(n)) {
+                                m = false;
+                            }
+                        }
+
+                        if (q1 <= q11 && w11 <= w1 && !r1.contains(q11) && m) {
                             long b = Duration.between(i11.get(x), i22.get(x)).toMillis();
                             statisticInfo.selfTime = (int) (statisticInfo.selfTime - b);
+                            r1.add(q11);
+                            r2.add(w11);
                         }
                     }
                 }
+
+
             }
 
             list.add(statisticInfo);
